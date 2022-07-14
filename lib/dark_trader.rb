8de75798b2cba7ea0a3e9fb947bec8e556de1400
linahello <tropikalmoon@gmail.com>
2022-07-14 17:24:51 +0200

    exos 1, 2 et 3

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,4 +1,96 @@
+require 'rubygems'
+  require 'open-uri'
+    require 'nokogiri'
+
+    doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+    doc.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[2]/div/a[1]').each do |node| //*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[2]/div/a[1]
+      puts node.text
+    end
+=begin
+page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))   
+puts page.class   # => Nokogiri::HTML::Document
+
+Xpath
+
+=begin
+crypto_name_array
+crypto_nameS .each
+Rappel: un hash s’initialise avec result = Hash.new 
+et on y stocke des infos avec result['ta_key'] = 'ta_value'
+
+a = [
+  { "BTC" => 5245.12 },
+  { "ETH" => 217.34 }, 
+  etc
+]
+
+=end
+
+def scraping(url)
+  html = open("#{url}").read
+  nokogiri_doc = Nokogiri::HTML(html)
+  final_array = []
+
+  nokogiri_doc.search("a span").each do |element|
+    element = element.text
+    final_array << element 
+  end
+
+  final_array.each_with_index do |index, element|
+    puts "#{index + 1} - #{element}"
+  end
+end
+
+scraping('https://coinmarketcap.com/all/views/all/')
+
+
+
+
+
+
+
 require 'nokogiri'
-  require 'open-url'
-    require 'dotenv'# Appelle la gem Dotenv
-      require 'pry'
+require 'open-uri'
+
+def define_page
+  page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+end
+
+def extract_crypto
+  page = define_page
+  list_cryptos = []
+
+  page.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody //div/a[contains(@class, "cmc-table__column-name--symbol")]').each do |crypto|
+    list_cryptos.push(crypto.text)
+  end
+  return list_cryptos
+end
+
+def extract_values
+  page = define_page
+  list_values = []
+
+  page.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody //td[contains(@class, "cmc-table__cell--sort-by__price")]/div/a/span').each do |value|
+    list_values.push(value.text.gsub(/[$,]/, '').to_f)
+  end
+  return list_values
+end
+
+def create_final_array
+
+  list_cryptos = extract_crypto
+  list_values = extract_values
+  final_list = []
+  array_length = list_cryptos.length
+
+  for i in 0..(array_length - 1) 
+    h = {list_cryptos[i]=> list_values[i]}
+    final_list.push(h)
+  end
+
+  return final_list
+end
+
+final_list = create_final_array
+puts final_list
+
--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,1 +1,53 @@
 require_relative '../lib/dark_trader'
+
+1) Récupérer le cours de toutes les cryptomonnaies
+Isoler les éléments HTML qui vont bien, 
+
+2) les enregistrer dans un array de hashs
+En extraire le texte et mettre ça dans un hash, 
+
+3) Réorganiser ce hash dans un array de plusieurs mini-hash comme demandé.
+Même si ça n'est pas le chemin le plus court, l'essentiel est que chaque petite 
+étape te fasse avancer et qu'à chaque fois tu te dises "ok, étape 1), ça fonctionne nickel -
+ pas de bug. Passons à la suite".
+
+ un array crypto 
+ un array value 
+
+
+
+
+ require_relative '../lib/dark_trader'
+
+describe 'we are getting all cryptos we can get' do
+  it 'vérify if there is 20 cryptos' do
+    expect(extract_crypto.length).to eq(20)
+  end
+  it 'verify that there is some current crypto' do
+    expect(extract_crypto.include?("BTC")).to eq(true)
+    expect(extract_crypto.include?("ETH")).to eq(true)
+  end
+end
+
+describe 'we are getting all the values we can get and in the right format' do
+  it 'vérify if there is 20 values' do
+    expect(extract_values.length).to eq(20)
+  end
+  it 'verify that they are floats' do
+    expect(extract_values[0].class).to eq(Float)
+    expect(extract_values[10].class).to eq(Float)
+  end
+end
+
+describe 'we have at the end an array of 20 hash' do
+  it 'verify that the fnal list is an array' do
+    expect(create_final_array.class).to eq(Array)
+  end
+  it 'verify that there are 20 elements' do 
+    expect(create_final_array.length).to eq(20)
+  end
+  it 'verify that elements are hashes' do
+    expect(create_final_array[0].class).to eq(Hash)
+    expect(create_final_array[10].class).to eq(Hash)
+  end
+end
